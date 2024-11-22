fa68a8809fd0e05f3753a271494e5a2954bcff7f
Anas <anasginolt@gmail.com>
2024-11-22 11:14:27 +0530

    user update completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -107,4 +107,33 @@ const followUnFollowUser = async (req, res) => {
 		console.log("Error in followUnFollowUser: ", err.message);
 	}
 };
-export {signupUser,loginUser,logoutUser,followUnFollowUser}
+
+const updateUser = async (req,res) => {
+    const {name,email,username,password,profilePic,bio} =req.body
+        const userId = req.user._id;
+    try {
+        let user = await User.findById(userId);
+        if(!user) return res.status(400).json({message:"User not found"});
+
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password,salt)
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+
+        user = await user.save();
+        res.status(200).json({message:"Profile updated successfully",user})
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        console.log("Error in updateUser :",error);
+        
+    }
+}
+export {signupUser,loginUser,logoutUser,followUnFollowUser,updateUser}
