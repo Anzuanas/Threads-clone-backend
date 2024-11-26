634a90f2626e801bfcc14ebc2cfe65b41cfc5a1c
Anas <anasginolt@gmail.com>
2024-11-26 23:56:59 +0530

    reply feature completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -88,4 +88,30 @@ const  likeUnlikePost = async (req,res) =>{
      }
 }
 
-export {createPost,getPost,deletePost,likeUnlikePost}
+const replyToPost = async (req,res) => {
+
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+        const userProfilePic = req.user.profilePic;
+        const username = req.user.username;
+
+        if(!text){
+            return res.status(400).json({message:err.message})
+        }
+        const post = await Post.findById(postId);
+        if(!post) {
+            return res.status(404).json({message:"Post not found"})
+        }
+        const reply = {userId, text, userProfilePic, username
+        }
+
+        post.replies.push(reply);
+        await post.save();
+        res.status(200).json({message:"reply added successfully",post})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+export {createPost,getPost,deletePost,likeUnlikePost,replyToPost}
