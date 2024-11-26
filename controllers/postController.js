aeaf7f7457118188974d5b6f279d0d8a99876109
Anas <anasginolt@gmail.com>
2024-11-26 21:07:30 +0530

    getpost and deletePost completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -34,4 +34,32 @@ const createPost = async (req,res) => {
     }
 }
 
-export {createPost}
+const getPost = async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            res.status(404).json({message:"Post not found"})
+        }
+        res.status(200).json({post})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+const deletePost = async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {return res.status(404).json({message:"Post not found"})}
+        if(post.postedBy.toString() !== req.user._id.toString()){
+            return res.status(401).json({message:"Unauthorized to delete the post"});
+        }
+
+        await Post.findByIdAndDelete(req.params.id)
+        res.status(200).json({message:"post deleted successfully"})
+    
+        } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+}
+
+export {createPost,getPost,deletePost}
