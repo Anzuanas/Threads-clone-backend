1043778972037b311d827425ce9a6f1d650677f4
Anas <anasginolt@gmail.com>
2024-11-27 12:08:51 +0530

    Get feed  completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -114,4 +114,20 @@ const replyToPost = async (req,res) => {
         res.status(500).json({message:error.message})
     }
 }
-export {createPost,getPost,deletePost,likeUnlikePost,replyToPost}
+
+const getFeedPosts = async (req,res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const following = user.following;
+        const feedPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt: -1});
+        res.status(200).json({feedPosts})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+export {createPost,getPost,deletePost,likeUnlikePost,replyToPost,getFeedPosts}
